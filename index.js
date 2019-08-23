22d63f1fb3fe0ecc16eb9a35e69acf99d6f33d8c
Miracle Chinedum <miraclechinedum16@gmail.com>
2019-08-23 13:24:36 +0100

    commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// let test = document.getElementById("input-text");
+
+// console.log("value==", test.value);
+
+function test() {
+  let text = document.getElementById("input-text");
+  let newtext = text;
+  console.log("text", text.value);
+  console.log("newtext", newtext.value);
+}
